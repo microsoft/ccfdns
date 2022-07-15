34b9f6b5c968ffa6f76856b89bc9ba1c4da6037e
Christoph M. Wintersteiger <cwinter@microsoft.com>
2022-07-15 16:17:55 +0000

    Add NSEC3PARAM format definition

--- rfc5155.h
+++ rfc5155.h
@@ -12,7 +12,8 @@ namespace RFC5155 // https://datatracker.ietf.org/doc/html/rfc5155
 {
   enum class Type : uint16_t
   {
-    NSEC3 = 50
+    NSEC3 = 50,
+    NSEC3PARAM = 51
   };
 
   inline std::map<Type, std::string> type_string_map = {{Type::NSEC3, "NSEC3"}};
@@ -90,12 +91,68 @@ namespace RFC5155 // https://datatracker.ietf.org/doc/html/rfc5155
     virtual operator std::string() const override
     {
       std::string r;
-      r = std::to_string((uint8_t)hash_algorithm);
+      r = std::to_string(static_cast<uint8_t>(hash_algorithm));
       r += " " + std::to_string(flags);
       r += " " + std::to_string(iterations);
       r += " " + ds::to_hex(salt);
       r += " " + next_hashed_owner_name.to_base32hex();
       r += " " + (std::string)type_bit_maps;
+      return r;
+    }
+  };
+
+  class NSEC3PARAM : public RFC1035::RDataFormat
+  {
+  public:
+    RFC4034::DigestType hash_algorithm;
+    uint8_t flags;
+    uint16_t iterations;
+    small_vector<uint8_t> salt;
+
+    NSEC3PARAM() = default;
+
+    NSEC3PARAM(const std::string& s)
+    {
+      std::istringstream f(s);
+      uint8_t tmp;
+      f >> tmp;
+      hash_algorithm = static_cast<RFC4034::DigestType>(tmp);
+      f >> flags;
+      f >> iterations;
+      std::string stmp;
+      f >> stmp;
+      salt = small_vector<uint8_t>::from_hex(stmp);
+    }
+
+    NSEC3PARAM(const small_vector<uint16_t>& data)
+    {
+      size_t pos = 0;
+      hash_algorithm =
+        static_cast<RFC4034::DigestType>(get<uint8_t>(data, pos));
+      flags = get<uint8_t>(data, pos);
+      iterations = get<uint16_t>(data, pos);
+      salt = small_vector<uint8_t>(data, pos);
+    }
+
+    virtual ~NSEC3PARAM() = default;
+
+    virtual operator small_vector<uint16_t>() const override
+    {
+      std::vector<uint8_t> r;
+      put((uint8_t)hash_algorithm, r);
+      put(flags, r);
+      put(iterations, r);
+      salt.put(r);
+      return small_vector<uint16_t>(r.size(), r.data());
+    }
+
+    virtual operator std::string() const override
+    {
+      std::string r;
+      r = std::to_string(static_cast<uint8_t>(hash_algorithm));
+      r += " " + std::to_string(flags);
+      r += " " + std::to_string(iterations);
+      r += " " + ds::to_hex(salt);
       return r;
     }
   };
