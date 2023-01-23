55944a27c6bf0848d1c57c7184678796d72e9a04
Christoph M. Wintersteiger <cwinter@microsoft.com>
2023-01-23 12:35:39 +0000

    Add tests.sh

---
+++ tests/tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache 2.0 License.
+
+set -e
+
+echo "Setting up Python environment..."
+if [ ! -f "env/bin/activate" ]
+    then
+        python3.8 -m venv env
+fi
+
+CCF=/data/cwinter/installed/ccf
+
+source env/bin/activate
+pip install -U -q pip
+pip install -U -q ccf
+pip install -q -U -r ../tests/requirements.txt
+echo "Python environment successfully setup"
+
+# Export where the VENV has been set, so tests running
+# a sandbox.sh can inherit it rather create a new one
+VENV_DIR=$(realpath env)
+export VENV_DIR="$VENV_DIR"
+
+# Enable https://github.com/Qix-/better-exceptions
+export BETTER_EXCEPTIONS=1
+
+export PYTHONPATH=$PYTHONPATH:$(grep ccf_.*_DIR CMakeCache.txt | cut -d = -f 2)/../bin
+
+ctest "$@"
