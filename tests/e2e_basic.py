857fb9d78e14c438c9cae686d9de5f3a0060a729
Max Tropets <maxtropets@gmail.com>
2025-06-12 14:16:35 +0000

    Reshuffle

--- tests/e2e_basic.py
+++ tests/e2e_basic.py
@@ -427,28 +427,29 @@ def test_service_registration(network, args):
         with_key=service_key,
     )
 
+    if args.enclave_platform == "virtual":
+        return
+
+    # Different service name should fail, no policy for it.
+    register_failed(
+        "no service relying party policy",
+        primary,
+        enclave=enclave,
+        service_name="another.acidns10.attested.name.",
+        with_key=service_key,
+    )
+
     # Register under wrong service registration policy (modified host data, aka security policy).
-    if args.enclave_platform != "virtual":
-
-        # Different service name should fail, no policy for it.
-        register_failed(
-            "no service relying party policy",
-            primary,
-            enclave=enclave,
-            service_name="another.acidns10.attested.name.",
-            with_key=service_key,
-        )
-
-        set_relying_party_policy_successfully(
-            network, enclave, service_name="test.acidns10.attested.name.", good=False
-        )
-        register_failed(
-            "Policy not satisfied",
-            primary,
-            enclave=enclave,
-            service_name="test.acidns10.attested.name.",
-            with_key=service_key,
-        )
+    set_relying_party_policy_successfully(
+        network, enclave, service_name="test.acidns10.attested.name.", good=False
+    )
+    register_failed(
+        "Policy not satisfied",
+        primary,
+        enclave=enclave,
+        service_name="test.acidns10.attested.name.",
+        with_key=service_key,
+    )
 
 
 def test_policy_registration(network, args):
